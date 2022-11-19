169767529deb44f944d06164d7369b8362b1a523
august-s-tk22 <august.spernjak.gbg22@amerikanskagymnasiet.se>
2022-11-19 15:30:39 +0100

    update

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+// Here I did it so that the images move with the mouse. The swiper detects mouse movement and moves the images along with it.
+
 const carousel = document.querySelector(".carousel");
 
-// Here I did it so that the images move with the mouse (sliding).
+let isDragStart = false, prevPageX, prevScrollLeft;
 
-const dragging = (e) => {
-    console.log(e.pageX); 
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX
+    prevScrollLeft = carousel.scrollLeft;
 }
 
+// pageX returns the horizontal coordinate of the mouse pointer.
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault(); 
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff; 
+}
+
+const dragStop = () => {
+    isDragStart = false;
+}
+
+// By default isDragStart is false and will only allow if the mouse button is held down.
+
+carousel.addEventListener("mousedown", dragStart); 
 carousel.addEventListener("mousemove", dragging); 
+carousel.addEventListener("mouseup", dragStop); 
