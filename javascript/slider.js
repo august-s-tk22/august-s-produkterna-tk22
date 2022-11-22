97fbcb31544149f43900a7bb9f81583c376f8f8b
august-s-tk22 <august.spernjak.gbg22@amerikanskagymnasiet.se>
2022-11-22 12:07:27 +0100

    update

--- javascript/slider.js
+++ javascript/slider.js
@@ -2,29 +2,31 @@
 
 const carousel = document.querySelector(".carousel");
 
-let isDragStart = false, prevPageX, prevScrollLeft;
+let isDragStart = false,
+  prevPageX,
+  prevScrollLeft;
 
 const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX
-    prevScrollLeft = carousel.scrollLeft;
-}
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
 
 // pageX returns the horizontal coordinate of the mouse pointer.
 
 const dragging = (e) => {
-    if(!isDragStart) return;
-    e.preventDefault(); 
-    let positionDiff = e.pageX - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff; 
-}
+  if (!isDragStart) return;
+  e.preventDefault();
+  let positionDiff = e.pageX - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+};
 
 const dragStop = () => {
-    isDragStart = false;
-}
+  isDragStart = false;
+};
 
 // By default isDragStart is false and will only allow if the mouse button is held down.
 
-carousel.addEventListener("mousedown", dragStart); 
-carousel.addEventListener("mousemove", dragging); 
-carousel.addEventListener("mouseup", dragStop); 
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
